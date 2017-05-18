14eb5cc73ef777385122aa47208f163e29a94a99
PrashantVaidyanathan <prash@bu.edu>
2017-05-18 18:10:49 -0400

    basic Spring added

---
+++ src/main/java/org/cidarlab/gridtli/Visualize/Webapp/MainController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cidarlab.gridtli.Visualize.Webapp;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author prash
+ */
+
+@Controller
+public class MainController {
+    
+}
