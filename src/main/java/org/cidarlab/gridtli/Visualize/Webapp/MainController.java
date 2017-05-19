84c11b9da935fd64b7961f27e019818b6a5ba573
PrashantVaidyanathan <prash@bu.edu>
2017-05-19 02:23:02 -0400

    can now send signals to the server

--- src/main/java/org/cidarlab/gridtli/Visualize/Webapp/MainController.java
+++ src/main/java/org/cidarlab/gridtli/Visualize/Webapp/MainController.java
@@ -5,7 +5,17 @@
  */
 package org.cidarlab.gridtli.Visualize.Webapp;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -14,5 +24,31 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MainController {
+    
+    
+    @RequestMapping(value="/getSTL", method=RequestMethod.POST)
+    public void getSTL(HttpServletRequest request, HttpServletResponse response){
+        String val = "";
+        System.out.println("I called you!");
+        
+        try {
+            JSONArray arr = new JSONArray(request.getParameter("signals"));
+            for(int i=0;i<arr.length();i++){
+                System.out.println(arr.get(i));
+            }
+        } catch (JSONException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                
+                
+        try {
+            PrintWriter writer = response.getWriter();
+            writer.print("Yayy");
+            writer.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
     
 }
